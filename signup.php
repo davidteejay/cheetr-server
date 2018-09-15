00040197392f39e3092ff821413496c3ffcac041
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2018-09-15 11:48:56 +0100

    Signup endpoint

--- signup.php
+++ signup.php
@@ -1,6 +1,8 @@
 <?php
 require_once('conn.php');
 
+$_POST = json_decode(file_get_contents("php://input"), true);
+
 $username = $_POST['username'];
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
@@ -8,18 +10,17 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $password = $_POST['password'];
 
-// $query = "SELECT * FROM users WHERE username = '$username'";
-// $stmt = $con->query($query);
-// $result = $stmt->fetch(PDO::FETCH_ASSOC);
+$query = "SELECT * FROM users WHERE username = '$username'";
+$stmt = $con->query($query);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// if (!empty($result)){
-//     echo "exists";
-// } else {
-//     $query = "INSERT INTO users VALUES (:id, :firstName, :lastName, :username, :email, :phone, :password, :loginRoute)";
-//     $stmt = $con->prepare($query);
-//     $stmt->execute(array(':id' => null, ':firstName' => $fname, ':lastName' => $lname, ':username' => $username, ':email' => $email, ':phone' => $phone, ':password' => $password, ':loginRoute' => null));
-//     echo "Success";
-// }
+if (!empty($result)){
+    echo "exists";
+} else {
+    $query = "INSERT INTO users VALUES (:id, :firstName, :lastName, :username, :email, :phone, :password, :loginRoute)";
+    $stmt = $con->prepare($query);
+    $stmt->execute(array(':id' => null, ':firstName' => $fname, ':lastName' => $lname, ':username' => $username, ':email' => $email, ':phone' => $phone, ':password' => $password, ':loginRoute' => null));
+    echo "Success";
+}
 
-echo json_encode([$username, $fname, $lname, $email, $phone, $password]);
 ?>
