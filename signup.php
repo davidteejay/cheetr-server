e6bb1b9a3636210a271fd892282d7a7263eb12f8
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2018-09-15 11:44:33 +0100

    Signup endpoint

--- signup.php
+++ signup.php
@@ -8,16 +8,18 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $password = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE username = '$username'";
-$stmt = $con->query($query);
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
+// $query = "SELECT * FROM users WHERE username = '$username'";
+// $stmt = $con->query($query);
+// $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if (!empty($result)){
-    echo "exists";
-} else {
-    $query = "INSERT INTO users VALUES (:id, :firstName, :lastName, :username, :email, :phone, :password, :loginRoute)";
-    $stmt = $con->prepare($query);
-    $stmt->execute(array(':id' => null, ':firstName' => $fname, ':lastName' => $lname, ':username' => $username, ':email' => $email, ':phone' => $phone, ':password' => $password, ':loginRoute' => null));
-    echo "Success";
-}
+// if (!empty($result)){
+//     echo "exists";
+// } else {
+//     $query = "INSERT INTO users VALUES (:id, :firstName, :lastName, :username, :email, :phone, :password, :loginRoute)";
+//     $stmt = $con->prepare($query);
+//     $stmt->execute(array(':id' => null, ':firstName' => $fname, ':lastName' => $lname, ':username' => $username, ':email' => $email, ':phone' => $phone, ':password' => $password, ':loginRoute' => null));
+//     echo "Success";
+// }
+
+echo json_encode([$username, $fname, $lname, $email, $phone, $password]);
 ?>
