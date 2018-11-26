5d772b549fa42feb0bbf53d5766ea78f75479ff3
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2018-11-26 22:21:48 +0100

    Bug fix

--- placeOrder.php
+++ placeOrder.php
@@ -4,7 +4,6 @@ require_once('conn.php');
 $_POST = json_decode(file_get_contents("php://input"), true);
 
 $userId = $_POST['userId'];
-$driverId = $_POST['driverId'];
 $recipientName = $_POST['recipientName'];
 $recipientPhone = $_POST['recipientPhone'];
 $pickup = $_POST['pickup'];
@@ -14,7 +13,7 @@ $carrier = $_POST['carrier'];
 
 $query = "INSERT INTO orders (userId, driverId, recipientName, recipientPhone, pickup, dropoff, selectedPackage, carrier) VALUES (:userId, :driverId, :recipientName, :recipientPhone, :pickup, :dropoff, :selectedPackage, :carrier)";
 $stmt = $con->prepare($query);
-$stmt->execute(array(':userId' => $userId, ':driverId' => $driverId, ':recipientName' => $recipientName, ':recipientPhone' => $recipientPhone, ':pickup' => $pickup, ':dropoff' => $dropoff, ':selectedPackage' => $selectedPackage, ':carrier' => $carrier));
+$stmt->execute(array(':userId' => $userId, ':driverId' => null, ':recipientName' => $recipientName, ':recipientPhone' => $recipientPhone, ':pickup' => $pickup, ':dropoff' => $dropoff, ':selectedPackage' => $selectedPackage, ':carrier' => $carrier));
 
 $id = $con->lastInsertId();
 echo 'Order placed ' . $id;
