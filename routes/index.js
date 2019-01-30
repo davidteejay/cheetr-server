e1807e8ec7cc9878a7191b1e56e33095fd155ced
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2019-01-30 12:29:25 +0100

    Bug fix

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const Auth = require('./Auth/')
 const Drivers = require('./Drivers/')
 const Orders = require('./Orders/')
 const Invoices = require('./Invoices/')
-const Cards = require('./Cards/')
+// const Cards = require('./Cards/')
 
 app.use('/auth', Auth)
 app.use('/drivers', Drivers)
 app.use('/orders', Orders)
 app.use('/invoices', Invoices)
-app.use('/cards', Cards)
+// app.use('/cards', Cards)
 
 module.exports = app
